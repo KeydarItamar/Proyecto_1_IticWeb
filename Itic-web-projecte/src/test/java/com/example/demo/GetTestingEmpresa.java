62e27e1ec40e5c2fa752ad3562e23a1b386f0f29
Oscar Pérez <2023_oscar.perez@iticbcn.cat>
2023-11-27 15:22:51 +0100

    test unitaris de get

---
+++ Itic-web-projecte/src/test/java/com/example/demo/GetTestingEmpresa.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.bean.Empresa;
+import com.example.demo.repository.EmpresaRepository;
+
+@SpringBootTest
+class GetTestingEmpresa {
+	
+	@Autowired
+	private EmpresaRepository bd;
+	
+	@Test
+	void testGetEmpresaById() {
+		Empresa empresa = new Empresa(999, "test", 999999999, "test@gmail.com", null);
+		Empresa resultat = bd.save(empresa);
+		Empresa empresaGet = bd.findById(resultat.getId()).orElse(null);
+		
+		assertEquals(resultat, empresaGet);
+	}
+	
+	@Test
+	void testGetAllEmpresas() {
+		Empresa empresa = new Empresa(999, "test", 999999999, "test@gmail.com", null);
+		ArrayList<Empresa> empresas = (ArrayList<Empresa>) bd.findAll();
+		Empresa resultat = bd.save(empresa);
+		empresas.add(resultat);
+		
+		assertEquals(empresas, (ArrayList<Empresa>) bd.findAll());
+	}
+	
+
+}
